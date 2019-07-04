9d35e514879b20d57fa69a8fb3a9ce341c8f6d43
Jack Woodmansey <jack.woodmansey@vibetickets.co.uk>
2019-07-04 20:35:02 +0100

    prod config

--- public/src/environments/environment.prod.ts
+++ public/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true,
+  api: {
+    // This gets proxied locally
+    url: '/api/',
+  },
+  google: {
+    maps: {
+      publicKey: 'AIzaSyC6ezApsld85tBilYDpsmBD-EakrGjl0fs',
+    },
+  },
 }
