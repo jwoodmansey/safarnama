ac102c047137d437e180a8a25e435ab1ab84cb79
Jack Woodmansey <jack.woodmansey@vibepay.com>
2021-09-05 14:01:55 +0100

    adding middleware for checking if user is admin of project for experience

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express'
 import { UserRepo } from '../model/repo/UserRepo'
 import { UserData } from '@common/user'
 import { ExperienceRepo } from '../model/repo/ExperienceRepo'
+import { ProjectRepo } from '../model/repo/ProjectRepo'
 
 export async function getAllUsers(_request: Request, response: Response) {
   const userRepo = new UserRepo()
@@ -49,4 +50,30 @@ export function ensureAdmin(request: Request, response: Response, next: NextFunc
 function isUserAdmin(request: Request): boolean {
   const userData = request.user as UserData
   return userData.roles !== undefined && userData.roles.includes('admin')
+}
+
+async function isUserAdminOfProjectForExperience(request: Request): Promise<boolean> {
+  const userData = request.user as UserData
+  // Admins can always access admin functions of ANY project
+  if (isUserAdmin(request)) {
+    return true
+  }
+  const experienceId = request.params.id
+  const experienceRepo = new ExperienceRepo()
+  const projectRepo = new ProjectRepo()
+  const [experience, projects] = await Promise.all([
+    experienceRepo.getModelById(experienceId),
+    projectRepo.getAllForUser(userData._id)
+  ])
+  if (!experience) {
+    return false
+  }
+  return experience.projects?.find(
+    experienceProject => projects.find(p =>
+      experienceProject == p._id &&
+      p.members?.find(m =>
+        m.roles.includes('admin') &&
+        m.userId === userData._id
+      ) !== undefined)
+  ) !== undefined
 }
