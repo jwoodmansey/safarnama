f385fcc21b371a815b8d611372a94e03db59a756
Jack Woodmansey <jack@emma-app.com>
2023-02-05 19:46:46 +0000

    WIP: table for editing translations

--- public/src/app/components/experience/translate-experience/translate-experience-dialog.component.ts
+++ public/src/app/components/experience/translate-experience/translate-experience-dialog.component.ts
@@ -1,8 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
+// import { MatDialogRef } from '@angular/material/dialog';
+// import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
 import { ExperienceService } from '@services/experience.service';
+
+// type TranslationString = {
+//   locale: string,
+//   translation: string
+// }
+
+// type Translation = {
+//   id: string
+//   // base: string;
+//   translations: TranslationString[],
+// }
 
 @Component({
   selector: 'app-translate-experience-dialog',
@@ -12,18 +24,39 @@ import { ExperienceService } from '@services/experience.service';
 export class TranslateExperienceDialogComponent implements OnInit {
 
   public form: FormGroup
+  public data = new MatTableDataSource<any>()
+
+  public displayedColumns: string[] = ['id']
+  public baseLocale = 'enGB'
 
   constructor(
     private fb: FormBuilder,
     private experienceService: ExperienceService,
-    private dialogRef: MatDialogRef<TranslateExperienceDialogComponent>,
-    private snackBar: MatSnackBar
+    // private dialogRef: MatDialogRef<TranslateExperienceDialogComponent>,
+    // private snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
-    // this.form = this.fb.group({
-    //   name: ['', Validators.required],
-    // })
+    this.displayedColumns = ['id', this.baseLocale, 'cyGB']
+
+    this.experienceService.getSelectedExperience().subscribe((experienceData) => {
+      this.data = new MatTableDataSource([
+        {
+          id: 'experience.name',
+          [this.baseLocale]: experienceData.name,
+        }, 
+        {
+          id: 'experience.description',
+          [this.baseLocale]: experienceData.description,
+        }
+      ])
+      this.form = this.fb.group(
+        this.data.data.reduce((prev, curr) => ({
+          ...prev,
+          [curr.id]: ['', Validators.required]
+      }), {}))
+    })
+
   }
 
   submit() {
